d4dfe8c3d0b6f6fbb2db40361505286f113951ec
AlexK98 <kot.oleksandr.v@gmail.com>
2019-03-16 12:00:04 +0200

    slight edits

---
+++ app/config/_defines.php
@@ -0,0 +1,16 @@
+<?php
+
+define('STATE_COMPLETED', 'completed');
+define('STATE_ACTIVE', 'active');
+
+define('COLUMN_ID', 'id');
+define('COLUMN_NAME', 'username');
+define('COLUMN_EMAIL', 'email');
+define('COLUMN_STATE', 'state');
+define('DIR_ASCEND', 'ASC');
+define('DIR_DESCEND', 'DESC');
+define('ITEMS_PER_PAGE', 3);
+
+define('COOKIE_LIFETIME', 31536000);
+
+define('FIELD_REQUIRED', 'Fields can not be empty.');
--- index.php
+++ index.php
@@ -1,24 +1,9 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
-define('STATE_COMPLETED', 'completed');
-define('STATE_ACTIVE', 'active');
-
-define('COLUMN_ID', 'id');
-define('COLUMN_NAME', 'username');
-define('COLUMN_EMAIL', 'email');
-define('COLUMN_STATE', 'state');
-define('DIR_ASCEND', 'ASC');
-define('DIR_DESCEND', 'DESC');
-define('ITEMS_PER_PAGE', 3);
-
-define('COOKIE_LIFETIME', 31536000);
-
-define('FIELD_REQUIRED', 'Fields can not be empty.');
-
 define('debug', false);
 
+require_once 'app/config/_defines.php';
 require 'app/lib/AutoLoader.php';
 
 //=================================================
